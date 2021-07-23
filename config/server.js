c1cafcd70949f692a30c358174725a78f173cb22
DillonRampersad <dillonrampersad2000@gmail.com>
2021-07-23 14:55:57 -0400

    Backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0b71d509a75bbfac1d324316d0a8b840'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5dd65f31-7c83-4e23-aa7f-2542af60ae3d'
+};
